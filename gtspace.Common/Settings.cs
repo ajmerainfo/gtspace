13fa4c997bae9622ed73394255dcab9ce04324c0
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-05 12:19:29 +0000

    依然在架构中

---
+++ gtspace.Common/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtspace.Common
+{
+    /// <summary>
+    /// 网站公共设置
+    /// </summary>
+    public static class Settings
+    {
+
+    }
+}
---
+++ gtspace.Common/UrlRewriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtspace.Common
+{
+    /// <summary>
+    /// Url地址重写器
+    /// </summary>
+    public class UrlRewriter
+    {
+    }
+}
