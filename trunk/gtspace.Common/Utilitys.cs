0b68f52330106358fe2d3d1ec47cdbff815ce624
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-05 12:57:34 +0000

    依然还在架构, 现在架构到Web层了.

---
+++ trunk/gtspace.Common/Utilitys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtspace.Common
+{
+    /// <summary>
+    /// 工具访问类
+    /// </summary>
+    public class Utilitys
+    {
+
+    }
+}
