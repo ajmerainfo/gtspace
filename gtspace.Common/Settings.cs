a09049d7c3fa47945950640549a719bd5f25539a
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-16 09:09:12 +0000

    进一步架构, 并加入一些核心机制, 如地址重写, 日志记录等等

--- gtspace.Common/Settings.cs
+++ gtspace.Common/Settings.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using gtspace.Entity;
 
 namespace gtspace.Common
 {
@@ -12,78 +13,123 @@ namespace gtspace.Common
     /// </summary>
     public static class Settings
     {
-        #region 私有变量
+		#region 私有变量
 
-        /// <summary>
-        /// 网站的根域名, 如 : http://www.baidu.com/
-        /// </summary>
-        static string _rootUrl = string.Empty;
+		/// <summary>
+		/// 网站的根域名, 如 : http://www.baidu.com/
+		/// </summary>
+		static string _rootUrl = string.Empty;
 
-        /// <summary>
-        /// 网站的根目录, 如 : C:\wwwroot\
-        /// </summary>
-        static string _rootPath = string.Empty;
+		/// <summary>
+		/// 网站的根目录, 如 : C:\wwwroot\
+		/// </summary>
+		static string _rootPath = string.Empty;
 
-        /// <summary>
-        /// 获取数据库连接字符串
-        /// </summary>
-        static string _connectionString = string.Empty;
+		/// <summary>
+		/// 数据库连接字符串
+		/// </summary>
+		static string _connectionString = string.Empty;
 
-        /// <summary>
-        /// 系统错误页面的路径, 如 : ~/Error.html
-        /// </summary>
-        static string _errorPage = "~/Error.html";
+		/// <summary>
+		/// 系统错误页面的路径, 如 : ~/app_error.html
+		/// </summary>
+		static string _errorPage = "~/app_error.html";
 
-        #endregion
+		/// <summary>
+		/// Url地址重写规则列表
+		/// </summary>
+		static List<RewriteRule> _rewriteRules = null;
+
+		#endregion
 
 
-        #region 属性
-        /// <summary>
-         /// 网站的根域名, 如 : http://www.baidu.com/
-        /// </summary>
-         public static string RootUrl
-         {
-             get
-             {
-                 return _rootPath;
-             }
-             set
-             {
-                 _rootPath = value;
-             }
-         }
+		#region 属性
+		/// <summary>
+		/// 网站的根域名, 如 : http://www.baidu.com/
+		/// </summary>
+		public static string RootUrl
+		{
+		 get
+		 {
+			 return _rootPath;
+		 }
+		 set
+		 {
+			 _rootPath = value;
+		 }
+		}
 
-        /// <summary>
-         ///  网站的根目录, 如 : C:\wwwroot\
-        /// </summary>
-         public static string RootPath
-         {
-             get
-             {
-                 return _rootPath;
-             }
-             set
-             {
-                 _rootPath = value;
-             }
-         }
+		/// <summary>
+		///  网站的根目录, 如 : C:\wwwroot\
+		/// </summary>
+		public static string RootPath
+		{
+		 get
+		 {
+			 return _rootPath;
+		 }
+		 set
+		 {
+			 _rootPath = value;
+		 }
+		}
 
-        /// <summary>
-        /// 获取数据库连接字符串
-        /// </summary>
-         public static string ConnectionString
-         {
-             get
-             {
-                 //根据是否为调试模式调用连接字符串；
-                 return string.Empty;
-             }
-             set
-             {
-                 _connectionString = value;
-             }
-         }
+		/// <summary>
+		/// 获取数据库连接字符串
+		/// </summary>
+		public static string ConnectionString
+		{
+		 get
+		 {
+			 return _connectionString;
+		 }
+		 set
+		 {
+			 _connectionString = value;
+		 }
+		}
 
-        #endregion
+		/// <summary>
+		/// 获取系统错误页的路径
+		/// </summary>
+		public static string ErrorPage
+		{
+			get
+			{
+				return _errorPage;
+			}
+		}
+
+		/// <summary>
+		/// 设置或获取日志路径, 后面不包括 '\' 或 '/'
+		/// </summary>
+		public static string LogPath
+		{
+		 set
+		 {
+			 Utilitys.Log = new LogHelper(value);
+		 }
+		 get
+		 {
+			 return (Utilitys.Log == null) ? string.Empty : Utilitys.Log.LogPath;
+		 }
+		}
+
+		/// <summary>
+		/// 获取或设置Url地址重写规则列表
+		/// </summary>
+		public static List<RewriteRule> RewriteRules
+		{
+		 set
+		 {
+			 _rewriteRules = value;
+		 }
+		 get
+		 {
+			 return _rewriteRules;
+		 }
+		}
+
+		#endregion
     }
 }
--- gtspace.Common/Utilitys.cs
+++ gtspace.Common/Utilitys.cs
@@ -10,6 +10,24 @@ namespace gtspace.Common
     /// </summary>
     public class Utilitys
     {
+		/// <summary>
+		/// 日志帮助器
+		/// </summary>
+		public static LogHelper Log = null;
 
+		/// <summary>
+		/// JavaScript帮助器
+		/// </summary>
+		public static JavaScriptHelper JS = new JavaScriptHelper();
+
+		/// <summary>
+		/// 数据检验器
+		/// </summary>
+		public static ValidateHelper Validater = new ValidateHelper();
+
+		/// <summary>
+		/// 网页Url地址重写器
+		/// </summary>
+		public static UrlRewriter UrlRewriter = new UrlRewriter();
     }
 }
