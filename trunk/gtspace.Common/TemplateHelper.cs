45cf988080daea89165919deff9246bf85ab93c3
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-16 11:26:11 +0000

    模板机制开发中

---
+++ trunk/gtspace.Common/TemplateHelper.cs
@@ -0,0 +1,42 @@
+﻿/// Created by zwc at 2009年10月16日
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using gtspace.Entity;
+
+namespace gtspace.Common
+{
+	/// <summary>
+	/// 模板帮助器
+	/// </summary>
+	public class TemplateHelper
+	{
+		/// <summary>
+		/// 从一个模板配置文件里读取配置信息
+		/// </summary>
+		/// <param name="path">配置文件的物理绝对路径</param>
+		/// <returns>模板配置信息</returns>
+		public TemplateInfo LoadConfig(string path)
+		{
+			//
+			// TODO : 读取的代码留给后面搞
+			//
+
+
+			TemplateInfo info = new TemplateInfo();
+			info.Directory = "Default";
+			info.Name = "默认的模板";
+			info.Version = "0.1";
+			info.Author = "GTeam";
+			info.CopyRight = "Copyright 2009 gtspace Development Team";
+			info.ScreenShot = "screenshot.jpg";
+			info.Description = "这是个gtspace默认的模板";
+			info.RewriteRules = new List<RewriteRule>();
+
+			info.RewriteRules.Add(new RewriteRule() { From = "^default.aspx[?]$", To = "~/Templates/Default/index.aspx" });
+
+			return info;
+		}
+	}
+}
---
+++ trunk/gtspace.Entity/TemplateInfo.cs
@@ -0,0 +1,54 @@
+﻿/// Created by zwc at 2009年10月16日
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtspace.Entity
+{
+	/// <summary>
+	/// 模板信息
+	/// </summary>
+	public class TemplateInfo
+	{
+		/// <summary>
+		/// 模板所在的目录名
+		/// </summary>
+		public string Directory = string.Empty;
+
+		/// <summary>
+		/// 模板的名字
+		/// </summary>
+		public string Name = string.Empty;
+
+		/// <summary>
+		/// 版本号
+		/// </summary>
+		public string Version = string.Empty;
+
+		/// <summary>
+		/// 作者
+		/// </summary>
+		public string Author = string.Empty;
+
+		/// <summary>
+		/// 版权信息
+		/// </summary>
+		public string CopyRight = string.Empty;
+
+		/// <summary>
+		/// 略缩图
+		/// </summary>
+		public string ScreenShot = string.Empty;
+
+		/// <summary>
+		/// 描述
+		/// </summary>
+		public string Description = string.Empty;
+
+		/// <summary>
+		/// Url地址重写列表
+		/// </summary>
+		public List<RewriteRule> RewriteRules = null;
+	}
+}
