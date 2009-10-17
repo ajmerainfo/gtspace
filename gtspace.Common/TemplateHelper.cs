02faf85c874232bdcf3b8b2d00eb45d14ea48982
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-17 01:06:07 +0000

    把TemplateHelper里的LoadConfig改名为Load

--- gtspace.Common/TemplateHelper.cs
+++ gtspace.Common/TemplateHelper.cs
@@ -13,11 +13,11 @@ namespace gtspace.Common
 	public class TemplateHelper
 	{
 		/// <summary>
-		/// 从一个模板配置文件里读取配置信息
+		/// 从一个模板配置文件里读取配置信息, 如果配置文件不存在则返回null
 		/// </summary>
 		/// <param name="path">配置文件的物理绝对路径</param>
 		/// <returns>模板配置信息</returns>
-		public TemplateInfo LoadConfig(string path)
+		public TemplateInfo Load(string path)
 		{
 			//
 			// TODO : 读取的代码留给后面搞
@@ -37,6 +37,15 @@ namespace gtspace.Common
 			info.RewriteRules.Add(new RewriteRule() { From = "^default.aspx[?]$", To = "~/Templates/Default/index.aspx" });
 
 			return info;
+		}
+
+		/// <summary>
+		/// 读取所有模板信息
+		/// </summary>
+		/// <returns>模板列表</returns>
+		public List<TemplateInfo> LoadAll()
+		{
+			throw new NotImplementedException("没有写这个函数");
 		}
 	}
 }
