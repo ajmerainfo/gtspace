8c1c87a0cc9b26adc712295fa2e335aa59ff9d79
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-17 03:34:24 +0000

    添加了压缩帮助器的类定义和逻辑异常的类定义

---
+++ trunk/gtspace.Common/ZipHelper.cs
@@ -0,0 +1,34 @@
+﻿/// Created by zwc at 2009年10月17日
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtspace.Common
+{
+	/// <summary>
+	/// 压缩包帮助器
+	/// </summary>
+	public class ZipHelper
+	{
+		/// <summary>
+		/// 压缩一个文件夹为zip
+		/// </summary>
+		/// <param name="dir">需要压缩的文件夹路径</param>
+		/// <param name="zipFile">目标zip文件的路径</param>
+		public void Zip(string dir, string zipFile)
+		{
+			throw new NotImplementedException("没有实现的函数");
+		}
+
+		/// <summary>
+		/// 解压一个zip到一个文件夹
+		/// </summary>
+		/// <param name="zipFile">zip文件的路径</param>
+		/// <param name="dir">目标文件夹的路径</param>
+		public void UnZip(string zipFile, string dir)
+		{
+			throw new NotImplementedException("没有实现的函数");
+		}
+	}
+}
---
+++ trunk/gtspace.Entity/LogicException.cs
@@ -0,0 +1,21 @@
+﻿/// Author : 曾文超
+/// Create Date : 2009年7月7日
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glassesol.Entity
+{
+    /// <summary>
+    /// 逻辑错误, 由于用户的操作不当而发生的, 不影响系统正常
+    /// </summary>
+    public class LogicException : Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message">错误消息</param>
+        public LogicException(string message) : base(message) { }
+    }
+}
