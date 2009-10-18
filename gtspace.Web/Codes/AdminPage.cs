6e6285a38e9b2ecc87e668bff7133c9b066c2820
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-18 06:40:10 +0000

    去掉了登录页面, 打算把管理员和权限控制都做成插件了

---
+++ gtspace.Web/Codes/AdminPage.cs
@@ -0,0 +1,30 @@
+﻿/// Created by zwc at 2009年10月18日
+using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace gtspace.Web.Codes
+{
+	/// <summary>
+	/// 后台管理页面的基类, 所有需要权限控制的页面都需要继承这个类
+	/// </summary>
+	public class AdminPage : Page
+	{
+		/// <summary>
+		/// PageLoad函数之前执行的函数
+		/// </summary>
+		/// <param name="e"></param>
+		protected override void OnPreLoad(EventArgs e)
+		{ 
+			
+		}
+	}
+}
