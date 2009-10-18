434f3a274b5212912fcccced9e243139bb19978e
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-18 10:43:34 +0000

    将gtspace.AddOn.Default项目改为gtspace.Plugin.Default

---
+++ trunk/gtspace.Plugin.Default/PluginGlobal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using gtspace.Common.Contract;
+
+namespace gtspace.Plugin.Default
+{
+	public class PluginGlobal : IPlugin
+	{
+
+		#region IPlugin 成员
+
+		void IPlugin.Application_Start(object sender, EventArgs e)
+		{
+
+		}
+
+		void IPlugin.Session_Start(object sender, EventArgs e)
+		{
+			
+		}
+
+		void IPlugin.Application_BeginRequest(object sender, EventArgs e)
+		{
+
+		}
+
+		void IPlugin.Application_AuthenticateRequest(object sender, EventArgs e)
+		{
+			
+		}
+
+		void IPlugin.Application_Error(object sender, EventArgs e)
+		{
+			
+		}
+
+		void IPlugin.Session_End(object sender, EventArgs e)
+		{
+			
+		}
+
+		void IPlugin.Application_End(object sender, EventArgs e)
+		{
+			
+		}
+
+		void IPlugin.AdminPage_OnPreLoad(EventArgs e)
+		{
+			
+		}
+
+		#endregion
+	}
+}
---
+++ trunk/gtspace.Plugin.Default/index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using gtspace.Common;
+using gtspace.Web.Codes;
+
+namespace gtspace.Plugin.Default
+{
+	public partial class index : AdminPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Label1.Text = Utilitys.JS.Alert("网站根目录的物理路径是 : " + Settings.RootPath);
+
+		}
+	}   
+}
