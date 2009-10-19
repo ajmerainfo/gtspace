b1b54d5dbf236613e4aa76fd93c338382f9392c4
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-19 14:05:58 +0000

    将之前gtspace.Common的帮助器复原了

---
+++ gtspace.Common/ValidateHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtspace.Common
+{
+    /// <summary>
+    /// 验证帮助类
+    /// </summary>
+    public class ValidateHelper
+    {
+    }
+}
