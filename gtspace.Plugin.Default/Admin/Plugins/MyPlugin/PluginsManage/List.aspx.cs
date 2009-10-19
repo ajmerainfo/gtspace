5149d2309570695f5579b6bdd9c0297bbc6d82d2
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-19 12:20:31 +0000

    将gtspace.Plugin.Default作为模板与插件管理, 并修复了Admin.aspx不能显示当前主导航栏的BUG

--- gtspace.Plugin.Default/Admin/Plugins/MyPlugin/PluginsManage/List.aspx.cs
+++ gtspace.Plugin.Default/Admin/Plugins/MyPlugin/PluginsManage/List.aspx.cs
@@ -10,16 +10,15 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
-using gtspace.Common;
 using gtspace.Web.Codes;
 
-namespace gtspace.Plugin.Default.Admin.Plugins
+namespace gtspace.Plugin.Default.Admin.Plugins.MyPlugin.PluginsManage
 {
-	public partial class index : AdminPage
+	public partial class List : AdminPage
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			
+
 		}
 	}
 }
