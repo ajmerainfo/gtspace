8b1c44387341e233a04b13044163959158c69738
zwc1986 <zwc1986@dc3594f4-b0bf-11de-b547-437ca5cff257>
2009-10-20 01:31:59 +0000

    初步实现了插件的上传与删除

--- gtspace.Common/ZipHelper.cs
+++ gtspace.Common/ZipHelper.cs
@@ -19,15 +19,15 @@ namespace gtspace.Common
 		/// <param name="zipFile">目标zip文件的路径</param>
 		public void Zip(string dir, string zipFile)
 		{
-            string shellArguments = string.Format("x -o+ \"{0}\" \"{1}\\\"",zipFile, dir);
-            using (Process unrar = new Process())
-            {
-                unrar.StartInfo.FileName = "WinRar.exe";
-                unrar.StartInfo.Arguments = shellArguments;
-                unrar.Start();
-                unrar.WaitForExit();
-                unrar.Close();
-            }
+			string shellArguments = string.Format("a -o+ \"{0}\" \"{1}\\\"", zipFile, dir);
+			using (Process unrar = new Process())
+			{
+				unrar.StartInfo.FileName = "WinRar.exe";
+				unrar.StartInfo.Arguments = shellArguments;
+				unrar.Start();
+				unrar.WaitForExit();
+				unrar.Close();
+			}
 		}
 
 		/// <summary>
@@ -37,15 +37,15 @@ namespace gtspace.Common
 		/// <param name="dir">目标文件夹的路径</param>
 		public void UnZip(string zipFile, string dir)
 		{
-            string shellArguments = string.Format("a -o+ \"{0}\" \"{1}\\\"", zipFile, dir);
-            using (Process unrar = new Process())
-            {
-                unrar.StartInfo.FileName = "WinRar.exe";
-                unrar.StartInfo.Arguments = shellArguments;
-                unrar.Start();
-                unrar.WaitForExit();
-                unrar.Close();
-            }
+			string shellArguments = string.Format("x -o+ \"{0}\" \"{1}\\\"", zipFile, dir);
+			using (Process unrar = new Process())
+			{
+				unrar.StartInfo.FileName = "WinRar.exe";
+				unrar.StartInfo.Arguments = shellArguments;
+				unrar.Start();
+				unrar.WaitForExit();
+				unrar.Close();
+			}
 		}
 	}
 }
